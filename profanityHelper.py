2d9c1cbc52e62a84a6385a46697c8f1ee5bf62de
tacostahern@gmail.com <tacostahern@gmail.com>
2021-03-03 17:06:56 -0600

    initial creation of helper file

---
+++ profanityHelper.py
@@ -0,0 +1,10 @@
+'''
+This program will be used to help the profanity filter in Verse Travers
+'''
+
+def main():
+    repeat = input("Add more words? Enter 'Y' for yes: ")
+
+    while repeat.lower() == 'Y':
+        
+        
